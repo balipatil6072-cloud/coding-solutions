c630d1f664839c21384923ce1175d0bf65388a29
Balwant Baswade <balipatil6072@gmail.com>
2026-07-14 19:16:04 +0530

    ✅ Fibonacci Number | Easy | Java | Runtime: 9 ms (43.69%) | Memory: 42.2 MB | 2026-07-14

---
+++ leetcode/easy/0509-fibonacci-number/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int fib(int n) {
+        return fibo(n);
+    }
+    private int fibo(int n){
+        if(n<=1) return n;
+        else
+        return fibo(n-1)+fibo(n-2);
+    }
+}
